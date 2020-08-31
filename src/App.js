af5f4e9e36384c51b7a89b4aa0c82ff53c2f50fd
Shane Chintatkun <schintatkun@gmail.com>
2020-08-30 21:42:16 -0700

    completed basic files structure and initial components Form.js, TodoList.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import React from "react";
 import "./App.css";
+// Importing components
+import Form from "./components/Form";
+import TodoList from "./components/Form";
 
 function App() {
   return (
     <div className="App">
-      <h1>Initial React Project</h1>
+      <header>
+        <h1>ToDo List with ReactJS</h1>
+      </header>
+      <Form />
     </div>
   );
 }
